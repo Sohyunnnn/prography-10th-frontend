7a78bb83f74e5c46e4a232fae63351f654cae1db
Sohyunnnn <thgus05061@naver.com>
2025-02-19 17:22:12 +0900

    chore: install vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
 });
