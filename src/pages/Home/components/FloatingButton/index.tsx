1992f13c68778bdb983ede4c4ed4a791ad22ea63
Sohyunnnn <thgus05061@naver.com>
2025-02-19 17:32:50 +0900

    feat: 플로팅 버튼 추가 및 클릭 이벤트 추가

--- src/pages/Home/components/FloatingButton/index.tsx
+++ src/pages/Home/components/FloatingButton/index.tsx
@@ -1,10 +1,19 @@
 import { PlusIcon } from "@assets/svgs";
+import routes from "@constants/routes";
+import { useNavigate } from "react-router-dom";
 
 const FloatingButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(routes.recruiting);
+  };
+
   return (
     <button
       type="button"
-      className="relative flex cursor-pointer items-center rounded-4xl bg-white py-4 pr-9 pl-4 text-black"
+      className="fixed right-5 bottom-6 flex cursor-pointer items-center rounded-4xl bg-white py-4 pr-9 pl-4 text-black"
+      onClick={handleClick}
     >
       <span className="mr-7">지원하기</span>
       <div className="bg-primary absolute top-1/2 left-19 flex -translate-y-1/2 transform items-center justify-center rounded-full p-4 text-white">
