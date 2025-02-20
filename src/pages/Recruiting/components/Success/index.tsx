5682596d2f3439823ee56a29f81d188b9b23c2cc
Sohyunnnn <thgus05061@naver.com>
2025-02-20 19:43:36 +0900

    feat: 리크루팅 success 퍼블리싱

--- src/pages/Recruiting/components/Success/index.tsx
+++ src/pages/Recruiting/components/Success/index.tsx
@@ -1,8 +1,34 @@
+import { CheckIcon } from "@assets/svgs";
+import { generation } from "@constants/generation";
+import routes from "@constants/routes";
+import { useNavigate } from "react-router-dom";
 
 const Success = () => {
-  return (
-    <div>Success</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Success
+  const handleClick = () => {
+    navigate(routes.main);
+  };
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-5">
+      <div className="bg-primary flex h-15 w-15 items-center justify-center rounded-full">
+        <CheckIcon className="h-8 w-8" />
+      </div>
+      <h2 className="title2">지원이 완료되었습니다!</h2>
+      <p className="body1 text-center">
+        프로그라피 {generation.tenth}기에 지원해주셔서 감사합니다. <br />
+        서류 심사 결과는 입력하신 이메일로 안내드릴 예정입니다.
+      </p>
+      <button
+        type="button"
+        className="bg-primary rounded-md p-3"
+        onClick={handleClick}
+      >
+        홈으로 돌아가기
+      </button>
+    </div>
+  );
+};
+
+export default Success;
--- src/pages/Recruiting/index.tsx
+++ src/pages/Recruiting/index.tsx
@@ -9,7 +9,7 @@ import { generation } from "@constants/generation";
 const steps = [1, 2, 3];
 
 const Recruiting = () => {
-  const [step, setStep] = useState(3);
+  const [step, setStep] = useState(1);
 
   const handleNext = () => {
     if (step < steps.length + 1) setStep((prev) => prev + 1);
@@ -22,8 +22,10 @@ const Recruiting = () => {
   return (
     <main className="flex items-center">
       <div className="bg-gray-10 flex h-173 w-150 flex-col justify-between rounded-lg px-10 pt-6 pb-10">
-        <div>
-          <p className="title2-1 p-3 text-center">Prography {generation.tenth}기 지원서</p>
+        <p className="title2-1 p-3 text-center">
+          Prography {generation.tenth}기 지원서
+        </p>
+        <div className="h-full">
           <div className="flex items-center justify-center gap-2">
             {steps.map((currentStep) => (
               <React.Fragment key={currentStep}>
@@ -49,28 +51,30 @@ const Recruiting = () => {
           {step === 3 && <Step3 />}
           {step === 4 && <Success />}
         </div>
-        <div className="mt-0 flex justify-between">
-          <StepButton
-            onClick={handlePrev}
-            disabled={step === 1}
-            text="이전"
-            className={
-              step === 1
-                ? "bg-gray-300 text-gray-500"
-                : "bg-gray-500 text-white"
-            }
-          />
-          <StepButton
-            onClick={handleNext}
-            disabled={step === steps.length + 1}
-            text={step === steps.length ? "완료" : "다음"}
-            className={
-              step === steps.length + 1
-                ? "bg-gray-300 text-gray-500"
-                : "bg-primary text-white"
-            }
-          />
-        </div>
+        {step !== 4 && (
+          <div className="mt-0 flex justify-between">
+            <StepButton
+              onClick={handlePrev}
+              disabled={step === 1}
+              text="이전"
+              className={
+                step === 1
+                  ? "bg-gray-300 text-gray-500"
+                  : "bg-gray-500 text-white"
+              }
+            />
+            <StepButton
+              onClick={handleNext}
+              disabled={step === steps.length + 1}
+              text={step === steps.length ? "완료" : "다음"}
+              className={
+                step === steps.length + 1
+                  ? "bg-gray-300 text-gray-500"
+                  : "bg-primary text-white"
+              }
+            />
+          </div>
+        )}
       </div>
     </main>
   );
