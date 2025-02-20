5f2ac64cdd997d283a908c14769df38cfe1ec8e5
Sohyunnnn <thgus05061@naver.com>
2025-02-21 00:06:00 +0900

    feat: 이메일 유효성 검사 추가

--- src/pages/Recruiting/components/Step2/index.tsx
+++ src/pages/Recruiting/components/Step2/index.tsx
@@ -3,7 +3,21 @@ import FormHeader from "../FormHeader";
 import InputField from "./components/InputField";
 
 const Step2 = () => {
-  const { register } = useFormContext();
+  const {
+    register,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useFormContext();
+
+  const validateEmail = (email: string) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email)) {
+      alert("유효한 이메일 주소를 입력해주세요.");
+      return false;
+    }
+    return true;
+  };
 
   return (
     <>
@@ -12,7 +26,7 @@ const Step2 = () => {
         description="연락 가능한 정보를 입력해주세요."
       />
 
-      <div className="mt-7 flex flex-col gap-5">
+      <div className="mt-7 flex flex-col gap-8">
         <InputField
           id="name"
           label="성함을 입력해주세요."
@@ -20,13 +34,35 @@ const Step2 = () => {
           type="text"
           register={register("name")}
         />
-        <InputField
-          id="email"
-          label="이메일 주소를 입력해주세요."
-          placeholder="prography@gmail.com"
-          type="email"
-          register={register("email")}
-        />
+
+        <div>
+          <InputField
+            id="email"
+            label="이메일 주소를 입력해주세요."
+            placeholder="prography@gmail.com"
+            type="email"
+            register={register("email", {
+              onBlur: (e) => {
+                const email = e.target.value;
+                if (!validateEmail(email)) {
+                  setError("email", {
+                    type: "manual",
+                    message: "유효한 이메일 주소를 입력해주세요.",
+                  });
+                } else {
+                  clearErrors("email");
+                }
+              },
+            })}
+          />
+
+          {errors.email && typeof errors.email.message === "string" && (
+            <p className="caption1 absolute mt-1 text-sm text-red-500">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+
         <InputField
           id="phone"
           label="휴대폰 번호를 입력해주세요."
