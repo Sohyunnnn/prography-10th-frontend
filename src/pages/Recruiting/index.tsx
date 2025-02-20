b5519d20c056f718a0ef185e0ceea794376fb163
Sohyunnnn <thgus05061@naver.com>
2025-02-20 23:19:57 +0900

    style: 스타일 수정

--- src/pages/Recruiting/index.tsx
+++ src/pages/Recruiting/index.tsx
@@ -11,12 +11,7 @@ const steps = [1, 2, 3];
 
 const Recruiting = () => {
   const [step, setStep] = useState(1);
-  const methods = useForm({
-    defaultValues: {
-      privacyConsent: "",
-      field: "",
-    },
-  });
+  const methods = useForm();
 
   const handleNext = () => {
     if (step < steps.length) setStep((prev) => prev + 1);
